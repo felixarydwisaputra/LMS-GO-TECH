72b09de7778cfe847a44fe79dc4b17c110799803
feliksarydwisaputra <felixarydwisaputra@gmail.com>
2025-11-24 11:29:03 +0800

    Membuat structure folder awal

---
+++ src/pages/educator/StudentsEnrolled.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudentsEnrolled() {
+  return (
+    <div>StudentsEnrolled</div>
+  )
+}
---
+++ src/pages/student/MyEnrollment.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyEnrollment() {
+  return (
+    <div>MyEnrollment</div>
+  )
+}
